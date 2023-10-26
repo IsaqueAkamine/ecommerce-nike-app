01029d2dd7b41b7517e2f2db3c2e38c53409822b
Isaque Akamine <isaqueakamine@gmail.com>
2023-10-26 01:06:51 -0300

    feat: create products slice with redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
+import { Provider } from "react-redux";
+import { store } from "./src/store";
 import { Routes } from "./src/routes";
 
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <StatusBar style="auto" />
       <Routes />
-    </>
+    </Provider>
   );
 }
