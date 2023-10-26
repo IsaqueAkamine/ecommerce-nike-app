b1d7649a9c269a14551afdca084fda43a0a77437
Isaque Akamine <isaqueakamine@gmail.com>
2023-10-25 23:55:01 -0300

    feat: product detail screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import Products from "./src/screens/Products";
+import Details from "./src/screens/Details";
 
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <Products />
+      {/* <Products /> */}
+      <Details />
     </>
   );
 }
---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+import products from "../../data/products";
+
+import {
+  AddCartButton,
+  AddCartText,
+  Container,
+  Description,
+  Image,
+  ImageContainer,
+  InfoContainer,
+  Price,
+  ScrollContainer,
+  Title,
+} from "./styles";
+
+const Product = ({ uri }) => (
+  <ImageContainer>
+    <Image source={{ uri }} />
+  </ImageContainer>
+);
+
+const Details: React.FC = () => {
+  const product = products[0];
+
+  return (
+    <Container>
+      <ScrollContainer showsVerticalScrollIndicator={false}>
+        {/* Image Carousel */}
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => <Product uri={item} />}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          pagingEnabled
+        />
+
+        <InfoContainer>
+          {/* Title */}
+          <Title>{product.name}</Title>
+
+          {/* Price */}
+          <Price>$ {product.price}</Price>
+
+          {/* Description */}
+          <Description>{product.description}</Description>
+        </InfoContainer>
+      </ScrollContainer>
+
+      {/* Add to cart button */}
+      <AddCartButton>
+        <AddCartText>Add to cart</AddCartText>
+      </AddCartButton>
+
+      {/* Navigation icon */}
+    </Container>
+  );
+};
+
+export default Details;
